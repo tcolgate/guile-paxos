d4a4d97600c7e9d7aaa511810c05586c0535fc62
Tristan Colgate <tcolgate@gmail.com>
2013-03-10 20:42:46 +0000

    Tests for (majority N)

---
+++ tests/test-utils.scm
@@ -0,0 +1,26 @@
+(use-modules (paxos misc utils)
+             (srfi srfi-11))
+
+
+(test-begin "Utils tests") 
+
+
+(test-equal "majority - 1" (majority 1) 1)
+(test-equal "majority - 2" (majority 2) 2)
+(test-equal "majority - 3" (majority 3) 2)
+(test-equal "majority - 4" (majority 4) 3)
+(test-equal "majority - 5" (majority 5) 3)
+(test-equal "majority - 6" (majority 6) 4)
+(test-equal "majority - 7" (majority 7) 4)
+(test-equal "majority - 8" (majority 8) 5)
+
+(define passed
+  (if (eq? (test-runner-fail-count (test-runner-current)) 0)
+    0
+    1))
+
+(test-end "Utils tests") 
+
+(exit passed)
+
+

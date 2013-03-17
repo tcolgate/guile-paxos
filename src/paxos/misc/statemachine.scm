c7e3dbdeaf00764699d22d3da95580e5395412c8
Tristan Colgate <tcolgate@gmail.com>
2013-03-17 19:28:12 +0000

    Still not working, but may be getting there

--- src/paxos/misc/statemachine.scm
+++ src/paxos/misc/statemachine.scm
@@ -22,104 +22,134 @@
             ()
             ((_ forms ...)
              (letrec-syntax
+               ((process-automaton
+                  (lambda (stx)
+                    (syntax-case 
+                      stx ()
+                      ((_ initstate current next empty? isequal?
+                          (statespec (... ...))
+                          (... ...))
+                       (let
+                         ((specs (match (syntax->datum stx)
+                                        ((_ _ _ _ _ _ 
+                                            (spec (... ...)) 
+                                            (... ...))
+                                         spec)))) 
+                         (let ((states (map ; (statename (lambda...))
+                                         (lambda(spec)
+                                           (let-values 
+                                             (((sn n b a srs) 
+                                               (match spec 
+                                                      ((sn ': srs (... ...))
+                                                       (values 
+                                                         (datum->syntax stx sn)
+                                                         #f #f #f 
+                                                         (datum->syntax stx srs)))
+                                                      ((sn n b a ': srs (... ...))
+                                                       (values 
+                                                         (datum->syntax stx sn)
+                                                         n
+                                                         (datum->syntax stx sn)
+                                                         (datum->syntax stx srs))
+                                                       (values sn n b a srs)))))
 
-                 ((process-automaton
-                    (lambda (stx)
-                      (syntax-case
-                        stx
-                        (:)
-                        ((_ initstate current next empty? isequal?
-                            (statename : response (... ...))
-                            (... ...))
+                                             (if (and (integer? n)
+                                                      (> n 0))
+                                               #`(#,sn  
+                                                  (process-state-responses 
+                                                    #,sn 
+                                                    (quote #,sn) 
+                                                    current next empty? isequal? 
+                                                    #,@srs)) 
+                                               #`(#,sn 
+                                                  (process-state-responses 
+                                                    #,sn
+                                                    (quote #,sn)
+                                                    current next empty? isequal?
+                                                    #,@srs))
+                                               )))
+                                         specs)))
 
-                         #`(letrec ((statename
-                                      (process-state-responses
-                                        statename
-                                        (quote statename)
-                                        current next empty? isequal?
-                                        response (... ...)))
-                                    (... ...))
-                             initstate)))))
+                           (pretty-print (syntax->datum #`(letrec (#,@states) 
+                               initstate)))))))))
 
-                  (process-transition-test
-                    (syntax-rules
-                      ()
-                      ((_ C label isequal?)
-                       (isequal? label C))))
+                (process-transition-test
+                  (syntax-rules
+                    ()
+                    ((_ C label isequal?)
+                     (isequal? label C))))
 
-                  (process-transition-action
-                    (syntax-rules
-                      ()
-                      ((_ state targetfunc targetsym srcfunc srcsym next hooks)
-                       (targetfunc
-                         (next
-                           (fold
-                             apply
-                             state
-                             hooks
-                             (make-list (length hooks) (cons srcsym srcfunc))
-                             (make-list (length hooks) (cons targetsym targetfunc))))))))
+                (process-transition-action
+                  (syntax-rules
+                    ()
+                    ((_ state targetfunc targetsym srcfunc srcsym next hooks)
+                     (targetfunc
+                       (next
+                         (fold
+                           apply
+                           state
+                           hooks
+                           (make-list (length hooks) (cons srcsym srcfunc))
+                           (make-list (length hooks) (cons targetsym targetfunc))))))))
 
-                  (process-state-responses
-                    (lambda(stx)
-                      (syntax-case
-                        stx (accept abort alias ->)
-                        ((_ srcfunc
-                            srcsym
-                            current next empty? isequal?
-                            accept)
-                         #`(lambda(state)
-                             (values #t state (cons srcsym srcfunc))))
+                (process-state-responses
+                  (lambda(stx)
+                    (syntax-case
+                      stx (accept abort alias ->)
+                      ((_ srcfunc
+                          srcsym
+                          current next empty? isequal?
+                          accept)
+                       #`(lambda(state)
+                           (values #t state (cons srcsym srcfunc))))
 
-                        ((_ srcfunc
-                            srcsym
-                            current next empty? isequal?
-                            abort)
-                         #`(lambda(state)
-                             (values #f state (cons srcsym srcfunc)))
-                         )
+                      ((_ srcfunc
+                          srcsym
+                          current next empty? isequal?
+                          abort)
+                       #`(lambda(state)
+                           (values #f state (cons srcsym srcfunc))))
 
-                        ((_ srcfunc
-                            srcsym
-                            current next empty? isequal?
-                            alias target)
-                         #`(lambda(state)
-                             (target state))
-                         )
+                      ((_ srcfunc
+                          srcsym
+                          current next empty? isequal?
+                          alias target)
+                       #`(lambda(state)
+                           (target state)))
 
-                        ((_ srcfunc
-                            srcsym
-                            current next empty? isequal?
-                            (label -> target hooks (... ...)) (... ...))
-                         #`(process-state-responses
-                             srcfunc
-                             srcsym
-                             current next empty? isequal?
-                             (label -> target hooks (... ...)) (... ...)
-                             -> #f))
+                      ((_ srcfunc
+                          srcsym
+                          current next empty? isequal?
+                          (label -> target hooks (... ...)) (... ...))
+                       #`(process-state-responses
+                           srcfunc
+                           srcsym
+                           current next empty? isequal?
+                           (label -> target hooks (... ...)) (... ...)
+                           -> #f))
 
-                        ((_ srcfunc
-                            srcsym
-                            current next empty? isequal?
-                            (label -> target hooks (... ...)) (... ...)
-                            -> fallback)
-                         #`(lambda(state)
-                             (if (empty? state)
-                               (values #f state (cons srcsym srcfunc))
-                               (let ((c (current state)))
-                                 (cond
-                                   ((process-transition-test
-                                      c label isequal?)
-                                    (process-transition-action
-                                      state target (quote target) srcfunc srcsym next (list hooks (... ...))))
-                                   (... ...)
-                                   (else
-                                     #,(if (eq? #f (let* ((l (syntax->datum stx))
-                                                          (len (length l)))
-                                                     (list-ref l (- len 1))))
-                                         #`(values #f state)
-                                         #`(fallback (next state)))))))))))))
+                      ((_ srcfunc
+                          srcsym
+                          current next empty? isequal?
+                          (label -> target hooks (... ...)) (... ...)
+                          -> fallback)
+                       #`(lambda(state)
+                           (if (empty? state)
+                             (values #f state (cons srcsym srcfunc))
+                             (let ((c (current state)))
+                               (cond
+                                 ((process-transition-test
+                                    c label isequal?)
+                                  (process-transition-action
+                                    state target (quote target) srcfunc srcsym next (list hooks (... ...))))
+                                 (... ...)
+                                 (else
+                                   #,(if (eq? #f (let* ((l (syntax->datum stx))
+                                                        (len (length l)))
+                                                   (list-ref l (- len 1))))
+                                       #`(values #f state)
+                                       #`(fallback (next state)))))))))))))
 
-                 (process-automaton forms ...)))))
+               (process-automaton forms ...)))))
 
 
